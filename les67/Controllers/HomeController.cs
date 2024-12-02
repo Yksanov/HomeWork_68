f82182d28087fe7839c253875790814d6502fe6f
e.yksanov <e.yksanov@onoi.me>
2024-12-02 14:44:30 +0600

    added solution Test Lesson_68

---
+++ les67/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Main");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View("Policy");
+        }
+
+        public IActionResult TestPage(int page)
+        {
+            ViewBag.PageIncrement = ++page;
+            return View(new TestPageViewModel{Page = page});
+        }
+
+        
+    }
+}
